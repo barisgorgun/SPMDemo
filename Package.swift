64dd1617f4ab0691b61b395252a84465d208f9e1
baris.gorgun <barisgorgunn@gmail.com>
2022-03-16 11:37:17 +0300

    sync

--- Package.swift
+++ Package.swift
@@ -17,15 +17,10 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-       // .package(name: "WebRTC", url: "https://github.com/barisgorgun/kandy-ios-webrtc.git", branch: "SPMDemo")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-       /* .target(
-            name: "MobileSDK",
-            dependencies: ["WebRTC"],
-            path: "Sources"),*/
         .binaryTarget(name: "MobileSDKFramework",
                       path: "MobileSDK.xcframework"),
     ]
