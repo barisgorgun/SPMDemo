ac831e85e3d051f54ee18ae05e691d21d547c573
baris.gorgun <barisgorgunn@gmail.com>
2022-03-22 14:28:18 +0300

    added webrtc

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "MobileSDK",
-            targets: ["MobileSDKFramework","iOSLibrary"]),
+            targets: ["WebRTC","MobileSDKFramework","iOSLibrary"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -22,6 +22,9 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+                   name: "WebRTC",
+                   dependencies: [.product(name: "WebRTC", package: "SPMWebRTC")]),
         .target(
                     name: "iOSLibrary",
                     linkerSettings: [
---
+++ Sources/WebRTC/WebRTC.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Baris on 22.03.2022.
+//
+
+import Foundation
