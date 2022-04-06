95d783be2c84349eae510a8e6eadae164307edad
baris.gorgun <barisgorgunn@gmail.com>
2022-04-06 13:54:50 +0300

    deleted webrtc target

--- Package.swift
+++ Package.swift
@@ -12,19 +12,16 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "MobileSDK",
-            targets: ["WebRTC","MobileSDKFramework","iOSLibrary"]),
+            targets: ["MobileSDKFramework","iOSLibrary"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/barisgorgun/SPMWebRTC.git", branch: "packageFile"),
+        .package(name:"WebRTC" ,url: "https://github.com/barisgorgun/SPMWebRTC.git", branch: "packageFile"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "WebRTC",
-            dependencies: [.product(name: "WebRTC", package: "SPMWebRTC")]),
         .target(
                     name: "iOSLibrary",
                     cSettings: [
--- Sources/WebRTC/File.swift
+++
@@ -1,8 +0,0 @@
-//
-//  File.swift
-//  
-//
-//  Created by Baris on 17.03.2022.
-//
-
-import Foundation
