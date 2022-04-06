992b687c11f59e3669664a62cc71ea63ab59b1b0
baris.gorgun <barisgorgunn@gmail.com>
2022-04-06 14:38:07 +0300

    dependencies changes

--- Package.swift
+++ Package.swift
@@ -12,16 +12,19 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "MobileSDK",
-            targets: ["MobileSDKFramework","iOSLibrary"]),
+            targets: ["MobileSDK","MobileSDKFramework","iOSLibrary"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(name:"WebRTC" ,url: "https://github.com/barisgorgun/SPMWebRTC.git", branch: "packageFile"),
+        .package(url: "https://github.com/barisgorgun/SPMWebRTC.git", branch: "packageFile"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(name: "MobileSDK",
+                dependencies: [.product(name: "WebRTC", package: "SPMWebRTC")]),
+        
         .target(
                     name: "iOSLibrary",
                     cSettings: [
---
+++ Sources/MobileSDK/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Baris on 6.04.2022.
+//
+
+import Foundation
