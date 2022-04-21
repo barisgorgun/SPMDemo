22edae9653385d54093c98cfd6fa4da9ec4ecc70
baris.gorgun <barisgorgunn@gmail.com>
2022-04-21 21:59:19 +0300

    edited package.swift

--- Package.swift
+++ Package.swift
@@ -6,42 +6,41 @@ import PackageDescription
 let package = Package(
     name: "MobileSDK",
     platforms: [
-                .iOS(.v10)
-               ],
+        .iOS(.v11)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "MobileSDK",
-            targets: ["MobileSDK","MobileSDKFramework"]),
+            targets: ["MobileSDK","Dependencies","MobileSDKFramework"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/barisgorgun/SPMWebRTC.git", branch: "packageFile"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(name: "MobileSDK",
-                dependencies: [.product(name: "WebRTC", package: "SPMWebRTC")],
-                    cSettings: [
-                        .define("OTHER_LDFLAGS",to: "-ObjC"),
-                        .define("ENABLE_BITCODE", to: "NO")
-                    ],
-                    linkerSettings: [
-                        .linkedFramework("AudioToolbox"),
-                        .linkedFramework("VideoToolbox"),
-                        .linkedFramework("SystemConfiguration"),
-                        .linkedFramework("AVFoundation"),
-                        .linkedFramework("GLKit"),
-                        .linkedFramework("PushKit"),
-                        .linkedLibrary("c++"),
-                        .linkedLibrary("z"),
-                        .linkedLibrary("icucore"),
-                    ]),
+                dependencies: [.product(name: "WebRTC", package: "SPMWebRTC")]),
         
-        .binaryTarget(name: "MobileSDKFramework",
-                      path: "MobileSDK.xcframework"),
-       
+            .target(
+                name: "Dependencies",
+                cSettings: [
+                    .define("OTHER_LDFLAGS",to: "-ObjC"),
+                    .define("ENABLE_BITCODE", to: "NO")
+                ],
+                linkerSettings: [
+                    .linkedFramework("AudioToolbox"),
+                    .linkedFramework("VideoToolbox"),
+                    .linkedFramework("SystemConfiguration"),
+                    .linkedFramework("AVFoundation"),
+                    .linkedFramework("GLKit"),
+                    .linkedFramework("PushKit"),
+                    .linkedLibrary("c++"),
+                    .linkedLibrary("z"),
+                    .linkedLibrary("icucore"),
+                ]
+            ),
+        
+            .binaryTarget(name: "MobileSDKFramework",
+                          path: "MobileSDK.xcframework"),
+        
     ]
 )
---
+++ Sources/Dependencies/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Baris on 21.04.2022.
+//
+
+import Foundation
