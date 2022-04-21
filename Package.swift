affbd46a9f66ec08fb9493e4fd09789686a3c1af
baris.gorgun <barisgorgunn@gmail.com>
2022-04-21 22:45:16 +0300

    added packagefile

---
+++ Package.swift
@@ -0,0 +1,46 @@
+// swift-tools-version:5.5
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "MobileSDK",
+    platforms: [
+        .iOS(.v11)
+    ],
+    products: [
+        .library(
+            name: "MobileSDK",
+            targets: ["MobileSDK","Dependencies","MobileSDKFramework"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/barisgorgun/SPMWebRTC.git", branch: "packageFile"),
+    ],
+    targets: [
+        .target(name: "MobileSDK",
+                dependencies: [.product(name: "WebRTC", package: "SPMWebRTC")]),
+        
+            .target(
+                name: "Dependencies",
+                cSettings: [
+                    .define("OTHER_LDFLAGS",to: "-ObjC"),
+                    .define("ENABLE_BITCODE", to: "NO")
+                ],
+                linkerSettings: [
+                    .linkedFramework("AudioToolbox"),
+                    .linkedFramework("VideoToolbox"),
+                    .linkedFramework("SystemConfiguration"),
+                    .linkedFramework("AVFoundation"),
+                    .linkedFramework("GLKit"),
+                    .linkedFramework("PushKit"),
+                    .linkedLibrary("c++"),
+                    .linkedLibrary("z"),
+                    .linkedLibrary("icucore"),
+                ]
+            ),
+        
+            .binaryTarget(name: "MobileSDKFramework",
+                          path: "MobileSDK.xcframework"),
+        
+    ]
+)
